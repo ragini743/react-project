8e5e82cb777d2eceda955a09203b3df97943e567
Ragini <raginiv743@gmail.com>
2023-11-30 23:43:12 +0530

    create UserData

--- project-7/src/components/Contact.js
+++ project-7/src/components/Contact.js
@@ -6,7 +6,7 @@ const Contact = () => {
         <h1 className='font-extrabold text-4xl'>
             CONTACT US
         </h1>
-        <p
+        <p>LET’S CONNECT: WE’RE HERE TO HELP, AND WE’D LOVE TO HEAR FROM YOU! WHETHER YOU HAVE A QUESTION, COMMENT, OR JUST WANT TO CHAT , YOU CAN REACH OUT TO US THROUGH THE CONTACT FORM OF THIS PAGE, OR BY PHONE, EMAIL, OR SOCIAL MEDIA. </p>
     </div>
   )
 }
---
+++ project-7/src/components/UserData.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UserData = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default UserData
