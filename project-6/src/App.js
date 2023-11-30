cdc821a154e137d74ff8e03571fb753b2bd4b0f3
Ragini <raginiv743@gmail.com>
2023-11-30 23:21:50 +0530

    create header component

---
+++ project-6/src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="App bg-slate-400">
+   fddgujdilskzjdv
+    </div>
+  );
+}
+
+export default App;
---
+++ project-7/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <div className='w-[10%]'>
+            <img src='https://yt3.googleusercontent.com/ytc/APkrFKYuBowkxrmBN52Nt-LbGlrjZnCFagkkS_0yyLwP=s900-c-k-c0x00ffffff-no-rj'alt="logo"></img>
+
+        </div>
+    </div>
+  )
+}
+
+export default Header
