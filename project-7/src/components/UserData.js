b7ac9018e08aa64044b2591f33cf88d9d40b95b4
Ragini <raginiv743@gmail.com>
2023-11-30 23:46:50 +0530

    create button

--- project-7/src/components/UserData.js
+++ project-7/src/components/UserData.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 const UserData = () => {
   return (
-    <div></div>
+    <div>
+        <button type="button">
+            <div className='w-[10%]'>
+            <img src="https://previews.123rf.com/images/urfandadashov/urfandadashov1808/urfandadashov180823627/108990947-chat-vector-icon-isolated-on-transparent-background-chat-logo-concept.jpg" alt="chat"></img>
+            </div>VIA SUPPORT CHAT</button>
+        <button type="button"></button>
+    </div>
   )
 }
 
