36f05fa0c6df4d3296bd58f817337778794315a5
Ragini <raginiv743@gmail.com>
2023-12-01 09:32:38 +0530

    add icon

--- project-7/src/components/UserData.js
+++ project-7/src/components/UserData.js
@@ -7,7 +7,11 @@ const UserData = () => {
             <div className='w-[10%]'>
             <img src="https://previews.123rf.com/images/urfandadashov/urfandadashov1808/urfandadashov180823627/108990947-chat-vector-icon-isolated-on-transparent-background-chat-logo-concept.jpg" alt="chat"></img>
             </div>VIA SUPPORT CHAT</button>
-        <button type="button"></button>
+        <button type="button">
+            <div>
+                <img src="https://i.pinimg.com/originals/5a/df/75/5adf75d1b8b8503a132866d6330ff544.jpg"alt="logo"></img>
+            </div>
+        </button>
     </div>
   )
 }
