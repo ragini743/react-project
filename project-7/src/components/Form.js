3f08646854de8cfb891d11b3ca2f5b59ff93bc75
Ragini <raginiv743@gmail.com>
2023-12-01 11:42:37 +0530

    completed form

--- project-7/src/components/Form.js
+++ project-7/src/components/Form.js
@@ -2,18 +2,22 @@ import React from 'react'
 
 const Form = () => {
   return (
-    <div>
+    <div className='mt-6'>
       <form>
-      <div>
-        <input type='text' placeholder='Name'>
+      <div className='border-2 border-black relative p-3 mb-6'>
+        <label className='absolute -top-4 left-10 px-2 bg-white'>Name </label>
+        <input type='text'>
+          </input>
+         
+      </div> 
+      <div className='border-2 border-black relative p-3 mb-6'>
+      <label className='absolute -top-4 left-10 px-2 bg-white'>E-mail </label>
+        <input type='email'>
           </input>
       </div> 
-      <div>
-        <input type='email' placeholder='E-mail'>
-          </input>
-      </div> 
-      <div>
-        <textarea placeholder='TEXT'></textarea>
+      <div className='border-2 border-black relative p-3 mb-6'>
+      <label className='absolute -top-4 left-10 px-2 bg-white'>TEXT </label>
+        <textarea></textarea>
       </div> 
       </form> 
     </div>
