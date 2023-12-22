aea862aae57d2b03c10648e0080285f0c4ca8419
Ragini <raginiv743@gmail.com>
2023-12-22 20:02:06 +0530

    add button

--- project-8/src/components/Header.js
+++ project-8/src/components/Header.js
@@ -3,13 +3,13 @@ import { DiceImg } from '../Constant'
 const Header = () => {
   return (
     <div className='h-full flex'>
-        <img src={DiceImg} alt="Dice" className='h-full w-[70%] object-contain'>
+        <img src={DiceImg} alt="Dice" className='h-full w-[70%] object-contain bg-gradient-to-bl from-white to bg-slate-150'>
 
         </img>
         <div className='flex
          justify-center flex-col items-center'>
         <h1 className='text-6xl font-bold mb-4'>DICE GAME</h1>
-        <button className='border-black border-2 px-4 rounded-md'>Play now</button>
+        <button className='border-black border-2 px-4 rounded-md cursor-pointer bg-black text-white hover:bg-slate-400 hover:text-black '>Play now</button>
         </div>
     </div>
   )
