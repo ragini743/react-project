378655ce3bfa306b481f997033179f4af8c85efc
Ragini <raginiv743@gmail.com>
2023-12-24 14:49:17 +0530

    inside div add span and h1 tag

--- project-8/src/components/TotalScore.js
+++ project-8/src/components/TotalScore.js
@@ -2,8 +2,12 @@ import React from 'react'
 
 const TotalScore = () => {
   return (
-    <div>
-        <h1>Total Score</h1>
+    <div className='p-5 md:p-10'>
+        <div className='w-[30%]'>
+        <span className='border-2 border-black 
+        px-3 py-1 md:px-6 md:py-2 text-4xl font-bold'>0</span>
+        <h1 className='mt-5'>Total Score</h1>
+        </div>
     </div>
   )
 }
