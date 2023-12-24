1441c9cf5a9dd1b0163281abf35554f0f5b2636f
Ragini <raginiv743@gmail.com>
2023-12-24 14:31:54 +0530

    create TotalScore component

--- project-8/src/components/Body.js
+++ project-8/src/components/Body.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import TotalScore from './TotalScore'
 
 const Body = () => {
   return (
-    <div>xcvb </div>
+    <div>
+        <TotalScore />
+    </div>
   )
 }
 
---
+++ project-8/src/components/TotalScore.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TotalScore = () => {
+  return (
+    <div>
+        <h1>Total Score</h1>
+    </div>
+  )
+}
+
+export default TotalScore
