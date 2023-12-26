46ca5e560354d3066848247f71ec6cf371984c20
Ragini <raginiv743@gmail.com>
2023-12-26 14:57:00 +0530

    apply onclick event for clicking button

--- project-8/src/components/NumberSelector.js
+++ project-8/src/components/NumberSelector.js
@@ -1,14 +1,33 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const NumberSelector = () => {
-    const number=[1,2,3,4,5,6] ; 
-  return (
-    <div className='flex justify-evenly flex-wrap  md:w-[50%]'>
-{number.map((value) =>(
-<div key={value} className='border-2 border-black mt-2 px-6 h-[50px] py-3 font-bold'>{value}</div>
-    ))}
-    </div>
-  )
-}
+  const number = [1, 2, 3, 4, 5, 6];
+  const [selectedNumber, setSelectedNumber] = useState();
 
-export default NumberSelector
+//   console.log("sele", selectedNumber);
+
+  return (
+    
+    <div className="flex justify-evenly flex-wrap  md:w-[50%]">
+      {number.map((value) => (
+        <div
+          key={value}
+          onClick={() => {
+            setSelectedNumber(value);
+            console.log("sele", selectedNumber);
+          }}
+          style={{
+            background: value === selectedNumber ? "black" : "white",
+            color: value === selectedNumber ? "white" : "black",
+          }}
+          className="border-2  border-black mt-2 px-6 h-[50px] py-3 font-bold"
+        >
+          {value}
+        </div>
+      ))}
+    </div>
+    // <p>selectedNumber</p>
+     );
+};
+
+export default NumberSelector;
--- project-8/src/components/TotalScore.js
+++ project-8/src/components/TotalScore.js
@@ -6,7 +6,7 @@ const TotalScore = () => {
     <div className='p-5 md:p-10 flex flex-col md:flex-row md:justify-between'>
         <div className='mb-9 md:w-[30%] flex flex-col justify-center items-center'>
         <span className=' mx-auto border-2 border-black 
-        px-3 py-1 md:px-6 md:py-2 md:inline text-4xl font-bold'>0</span>
+        px-6 py-2 md:px-9 md:py-6 md:inline text-4xl md:text-6xl font-bold'>0</span>
         <h1 className='mt-5'>Total Score</h1>
         </div>
             <NumberSelector />
