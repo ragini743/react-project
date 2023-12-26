be8a4b7c0e35611392008381d682a4ffcaacf6ea
Ragini <raginiv743@gmail.com>
2023-12-26 16:08:02 +0530

    add two buttons ,and apply css

--- project-8/src/components/RollToDice.js
+++ project-8/src/components/RollToDice.js
@@ -2,11 +2,13 @@ import React from 'react'
 
 const RollToDice = () => {
   return (
-    <div>
-      <div className='flex justify-center mb-2'>
-        <img src="/dice_1.png" alt="dice-1"></img>
+    <div className='flex flex-col items-center'>
+      <div className='mb-2'>
+        <img src="/dice_1.png" className='w-[70%] m-auto' alt="dice-1"></img>
       </div>
       <p> click on Dice to roll</p>
+      <button type='reset' className='border-2 px-9 rounded-lg border-gray-700 mt-4 mb-3'>reset score</button>
+      <button type='submit' className='border-2 rounded-lg border-gray-700 px-8 mb-8'>show results</button>
     </div>
   )
 }
