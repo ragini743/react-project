9239942ac98b17eda99a48a25aa6e7d664e10561
Ragini <raginiv743@gmail.com>
2023-12-31 13:54:12 +0530

    create Modal and add useState in App.js

--- project-9/src/App.js
+++ project-9/src/App.js
@@ -5,8 +5,16 @@ import SearchContact from "./components/SearchContact";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "./config/Firebase";
 import GroupOfContact from "./components/GroupOfContact";
+import Modal from "./components/Modal";
 function App() {
   const [contact ,setContact] = useState([]) ;
+  const [open , setOpen] = useState(false);
+  const onOpen = () =>{
+    setOpen(true);
+  }
+  const onClose = () =>{
+    setOpen(false) ; 
+  }
   const getContacts = async () => {
     const contactCollection = collection (db, 'contacts'  );
     const snapshot = await getDocs(contactCollection);
@@ -30,12 +38,12 @@ function App() {
         <Header />
         <SearchContact />
         <NoContact />
-        
-          {contact.map((cont ) => (
-            <div key={cont.id} className="">
-          <GroupOfContact cont={cont} />
-          </div>))}
-      
+        <div  className="">
+          {contact.map((cont ) => ( 
+          <GroupOfContact key={cont.id} cont={cont}  />
+          ))}
+          </div>
+          <Modal open = {open} onClose= {onClose} />
     </div>
   );
 }
--- project-9/src/components/GroupOfContact.js
+++ project-9/src/components/GroupOfContact.js
@@ -3,13 +3,15 @@ import React from 'react';
 const GroupOfContact = ({cont}) => {
     console.log("cont",cont)
   return (
-    <div className='bg-yellow flex p-2 rounded-lg items-center justify-around'>
-        <div className='mr-0 '>
+    <div className='bg-yellow flex p-2  mb-4 rounded-lg items-center justify-between'>
+        <div className='flex '>
+        <div className='mr-4 '>
             <img src ="./user-circle-thin.png" alt="icon"></img>
         </div>
-        <div className=' mr-5'>
+        <div className=''>
             <p className='font-bold'>{cont.name}</p>
             <p className='font-normal'>{cont.email}</p>
+        </div>
         </div>
         <div className='flex'>
         <div className='mr-3'>
