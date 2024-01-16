cf09cd232dff0943723b100ea52b16e52a4c292e
Ragini <raginiv743@gmail.com>
2024-01-16 11:01:21 +0530

    create react app

---
+++ project-10/src/App.js
@@ -0,0 +1,14 @@
+import Slider from './component/Slider';
+import logo from './logo.svg';
+
+
+function App() {
+  return (
+    <div className="App bg-slate-900">
+      hello i'm header
+        <Slider />
+    </div>
+  );
+}
+
+export default App;
